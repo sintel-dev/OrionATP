1808317e1255bcea4cbbd13816d37d40d299b325
Avi Shah <avimshah@gmail.com>
2022-06-21 11:17:40 -0400

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -20,11 +20,11 @@ fig.add_trace(go.Scatter(x = anomalydata['timestamp'], y = anomalydata['value'],
                              name = 'detected_anomaly'))
 #fig.show() #graph figure with discrete anomalies highlighted in red
 st.header("Building the Pipeline")
-st.markdown("I'm not entirely sure, but I think Orion requires the data to be formatted in a very specific way, with the two columns of the dataframe being labelled 'timestamp' and 'value.' I set up the data as such, then build a pipeline that doesn't vary much from the default ARIMA pipeline that's preprogrammed into the Orion library.")
+st.markdown("Orion requires the data to be formatted in a very specific way, with the two columns of the dataframe being labelled 'timestamp' and 'value.' I set up the data to fit this format, then build a pipeline that doesn't vary much from the default ARIMA pipeline that's preprogrammed into the Orion library.")
 st.dataframe(selected)
 
 st.header("Detecting Anomalies")
-st.markdown("Once the Orion pipeline is constructed, I fit the model to the data and predict anomalous segments. The data is output into a dataframe representing the start and end of the anomalous sequence. Using a for loop, I split the sequence back into its original discrete data points that make it up.")
+st.markdown("Once the Orion pipeline is constructed, I fit the model to the data to train it and predict anomalous segments. The anomaly data is output into a dataframe representing the start and end of the anomalous sequence. Using a for loop, I split the sequence back into its original discrete data points that make it up in order to graph more effectively.")
 
 st.dataframe(detected_anomalies)
 
@@ -37,4 +37,4 @@ st.plotly_chart(fig)
 
 st.header("Conclusions")
 
-st.markdown("It's obviously difficult to draw any sort of conclusions based on just this data set, but it seems like the orion ARIMA pipeline is detecting anomalous segments during certain times of drastic increase in price. Obviously this script will change based on the orion model used and the data set. There's also far too many variables that weren't taken into account to even have an idea about what's going on here.")
+st.markdown("It's obviously difficult to draw any sort of conclusions based on just this data set, but it seems like the Orion ARIMA pipeline is detecting anomalous segments during certain times of drastic increase in price. Obviously this script will change based on the Orion model used and the data set. There's also far too many variables that weren't taken into account for us to be able to draw any definitive conclusions about causation based off this data.")
