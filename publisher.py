6d58e54eadabe4617e64a1842d163190094d2fbd
Avi Shah <avimshah@gmail.com>
2022-06-29 11:06:42 -0400

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -24,17 +24,23 @@ st.markdown("Orion requires the data to be formatted in a very specific way, wit
 st.dataframe(selected)
 
 st.header("Detecting Anomalies")
-st.markdown("Once the Orion pipeline is constructed, I fit the model to the data to train it and predict anomalous segments. The anomaly data is output into a dataframe representing the start and end of the anomalous sequence. Using a for loop, I split the sequence back into its original discrete data points that make it up in order to graph more effectively.")
+st.markdown("Once the Orion pipeline is constructed, I fit the model to the data to train it and predict anomalous segments. The anomaly data is output into a dataframe representing the start and end of the anomalous sequence. Using a for loop, I split the sequence back into its original discrete data points that make it up in order to graph more effectively. Below are the points within the anomalous segments detected along with a visualization on the graph.")
 
 st.dataframe(detected_anomalies)
 
 st.dataframe(anomalydata)
+
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x = anomalydata['timestamp'], y = anomalydata['value'], mode = 'markers',
+                             marker = dict(color='red'),
+                             name = 'detected_anomaly'))
+st.plotly_chart(fig2)
 
 st.header("Graphing the Data")
 st.markdown("Finally, I graph the data. I still need to familiarize myself with plotly to create more effective graphs, but this basic graph seems to work for now. Discrete anomalous points are highlighted with red dots, while the timeseries data is graphed over time with the blue line.") 
 
 st.plotly_chart(fig)
 
-st.header("Conclusions")
+st.header("Conclusions.")
 
 st.markdown("It's obviously difficult to draw any sort of conclusions based on just this data set, but it seems like the Orion ARIMA pipeline is detecting anomalous segments during certain times of drastic increase in price. Obviously this script will change based on the Orion model used and the data set. There's also far too many variables that weren't taken into account for us to be able to draw any definitive conclusions about causation based off this data.")
